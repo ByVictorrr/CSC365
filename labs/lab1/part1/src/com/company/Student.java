c0ef4c73210da9fef23ca9af71c42820b9f146eb
Victor Delaplaine <vdelaplaines@aol.com>
2020-01-06 14:51:31 -0800

    autoadd

---
+++ labs/lab1/part1/src/com/company/Student.java
@@ -0,0 +1,31 @@
+package com.company;
+
+class Student{
+    public String stLastName, stFirstName, tLastName, tFirstName;
+    public int grade, classRoom, busRoute;
+    public float gpa;
+
+
+    public Student(String stLastName, String stFirstName, int grade, int classRoom, int busRoute, float gpa, String tLastName, String tFirstName ) {
+        this.stLastName = stLastName;
+        this.stFirstName = stFirstName;
+        this.tLastName = tLastName;
+        this.tFirstName = tFirstName;
+        this.grade = grade;
+        this.classRoom = classRoom;
+        this.busRoute = busRoute;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return stLastName + ","
+                + stFirstName + ","
+                + gpa + ","
+                + tLastName + ","
+                + tFirstName + ","
+                + busRoute;
+
+    }
+
+}
