e6bd18d2981d03636beddf29f383c9537d13f5b2
Victor Delaplaine <vdelaplaines@aol.com>
2020-02-03 11:41:37 -0800

    autoadd

--- labs/lab3/AIRLINES/AIRLINES-4.sql
+++ labs/lab3/AIRLINES/AIRLINES-4.sql
@@ -7,27 +7,35 @@
 --          N-Z(inclusive): lat=36deg 50' N, long= 174deg 45E', alt=70
 
 -- ADD NEW COLUMNS
+
+
+ALTER TABLE Airports
+DROP COLUMN latitude,
+DROP COLUMN longitude,
+DROP COLUMN altitude;
+
+
 ALTER TABLE Airports 
 ADD COLUMN latitude FLOAT NOT NULL,
 ADD COLUMN longitude FLOAT NOT NULL,
 ADD COLUMN altitude INTEGER NOT NULL;
 
+
 -- Populate based above
 UPDATE Airports
-    SET latitude=39+(1/60)*50,  -- + is N
-        longitude=98+(35/60), -- + is W
+    SET latitude=39.9833,  -- + is N
+        longitude=98.5833, -- + is W
         altitude=42
 WHERE AirportCode >= "AAA" AND 
       AirportCode <= "MZZ";
 
 -- Populate based above
 UPDATE Airports
-    SET latitude=-36+(1/60)*50; -- - is S
-        longitude=-174+(45/60), -- E is -
+    SET latitude=-36.8333, -- - is S
+        longitude=-174.75, -- E is -
         altitude=70
 WHERE AirportCode >= "NAA" AND 
       AirportCode <= "ZZZ";
-
 
 
 
